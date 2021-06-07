71f6d17c3bc6989100f713cbe607a022e3ebbe23
Ren Ogaki <re.yuz77777@gmail.com>
2021-06-07 23:15:31 +0900

    Add a sample using gin

---
+++ gin/go.mod
@@ -0,0 +1,9 @@
+module github.com/yuzuy/go-framework-newrelic/gin
+
+go 1.16
+
+require (
+	github.com/gin-gonic/gin v1.7.2
+	github.com/newrelic/go-agent/v3 v3.0.0
+	github.com/newrelic/go-agent/v3/integrations/nrgin v1.1.0
+)
---
+++ gin/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"github.com/newrelic/go-agent/v3/integrations/nrgin"
+	"github.com/newrelic/go-agent/v3/newrelic"
+)
+
+func main() {
+	r := gin.New()
+	app, err := newrelic.NewApplication()
+	if err != nil {
+		log.Fatal(err)
+	}
+	r.Use(nrgin.Middleware(app))
+	if err := r.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
