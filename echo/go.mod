29c622a4fc48e445a3021c9a9a830fba11027984
Ren Ogaki <re.yuz77777@gmail.com>
2021-06-07 23:10:20 +0900

    Add a sample using echo

---
+++ echo/go.mod
@@ -0,0 +1,9 @@
+module github.com/yuzuy/go-framework-newrelic/echo
+
+go 1.16
+
+require (
+	github.com/labstack/echo/v4 v4.3.0
+	github.com/newrelic/go-agent/v3 v3.0.0
+	github.com/newrelic/go-agent/v3/integrations/nrecho-v4 v1.0.0
+)
---
+++ echo/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/newrelic/go-agent/v3/integrations/nrecho-v4"
+	"github.com/newrelic/go-agent/v3/newrelic"
+)
+
+func main() {
+	e := echo.New()
+	app, err := newrelic.NewApplication()
+	if err != nil {
+		e.Logger.Fatal(err)
+	}
+	e.Use(nrecho.Middleware(app))
+	e.Logger.Fatal(e.Start(":8080"))
+}
